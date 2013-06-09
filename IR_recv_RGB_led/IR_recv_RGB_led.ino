ced5b84ee62ff9fe4c3f8c568e4cac32c28fc01b
deffer <deffer@portal.(none)>
2013-06-09 22:26:40 +1200

    IR controlled LED

--- IR_recv_RGB_led/IR_recv_RGB_led.ino
+++ IR_recv_RGB_led/IR_recv_RGB_led.ino
@@ -1,31 +1,72 @@
 #include <IRremote.h>
 
-int volPlus =  0x807F8877;
-int volMinus = 0x807F08F7;
-int mute =     0x807F40BF;
-int input_up = 0x807F20DF;
-int reset_dn = 0x807F10EF;
-int treble_minus = 0x807FB04F;
-int treble_plus  = 0x807F906F;
-int bass_minus   = 0x807F708F;
-int bass_plus    = 0x807F50AF;
+long volPlus =  0x807F8877;
+long volMinus = 0x807F08F7;
+long mute =     0x807F40BF;
+long input_up = 0x807F20DF;
+long reset_dn = 0x807F10EF;
+long treble_minus = 0x807FB04F;
+long treble_plus  = 0x807F906F;
+long bass_minus   = 0x807F708F;
+long bass_plus    = 0x807F50AF;
 
 int repeat   = 0xFFFFFFFF;
 
+int REDpin = 3;
+int GREENpin = 5;
+int BLUEpin = 6;
+int br = 0;
+int incr  = 5;
+
+boolean BLUEenabled = false;
+boolean REDenabled =false;
 
 int RECV_PIN = 2;
 IRrecv irrecv(RECV_PIN);
 decode_results results;
 
+
+unsigned long previousMillis=0;
+
 void setup(){
+  pinMode(REDpin, OUTPUT);
+  pinMode(GREENpin, OUTPUT);
+  pinMode(BLUEpin, OUTPUT);
   Serial.begin(9600);
   irrecv.enableIRIn(); // Start the receiver
 }
 
 void loop() {
   if (irrecv.decode(&results)) {
+    if (results.value == volPlus)
+      BLUEenabled = true;
+    if (results.value == volMinus)
+      BLUEenabled = false;  
+      
+  if (results.value == mute)
+      REDenabled = !REDenabled;;
+      
     Serial.println(results.value, HEX);
     irrecv.resume(); // Receive the next value
+  }
+  
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= 30){
+    previousMillis = currentMillis;
+    br = br + incr;
+    if (br <= 0 || br >= 100){
+      incr = -incr;
+    }
+    
+    br = constrain(br, 0, 100);
+    if (BLUEenabled) 
+      analogWrite(BLUEpin, br);
+    else
+      analogWrite(BLUEpin, 0);
+    if (REDenabled) analogWrite(REDpin, br+155);
+    else analogWrite(REDpin, 0);
+    // analogWrite(GREENpin, br);
+    //analogWrite(REDpin, br+155);
   }
 }
 
