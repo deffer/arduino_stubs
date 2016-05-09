1c04ddbeab46c0f044e62bb9de2387d571565ec3
deffer <irina.benediktovich@gmail.com>
2016-05-09 22:45:55 +1200

    Programming motor to go up or down with the switch and sensor

---
+++ stepper_2motions_switch/stepper_2motions_switch.ino
@@ -0,0 +1,117 @@
+/**
+* 1. Stepper motor bipolar
+* 2. motor driver L293D
+* 3. button momentary - connects power (5V) to the EN1 and EN2 driver legs
+* 4. switch on-off-on to control the motor 
+*    on (top) - start routine UP (turn clockwise that many times)
+*    off (middle) - stop any ongoing routine
+*    on (bottom) - start routine DOWN
+* 5. reed switch - to detect the door is at the top (magnet is near the switch)
+*    LOW - switch is connected - magnet is near the switch - door at the at the top
+* 
+* When button number 3 is pressed it enabled power to the driver, which is moving stepper
+*   in the direction indicated by switch 4, which in turn moves the sliding door
+*   up or down. If switch is off (HIGH, door moves up), additional check is performed 
+*   - if reed switch reads LOW (connected), the door is at the top and we need 
+*   to stop the motor.
+*/
+#include <Stepper.h>
+
+const int stepsPerRevolution = 200;  
+const int MAX_STEPS_UP = stepsPerRevolution * 5;
+const int MAX_STEPS_DOWN = stepsPerRevolution * 5;
+
+// initialize the stepper library on pins 8 through 11:
+// 8 - IN1, 9 - IN2. other side: 10 - IN3, 11 - IN4
+// wires of one coil are connected to same side of the driver
+Stepper myStepper(stepsPerRevolution, 8,9,10,11);  
+
+// pins
+int PIN_TOP = 0;    // input pullup. LOW - door to go up
+int PIN_BOTTOM= 2;    // input pullup. LOW - door to go down
+int PIN_END_OF_TRACK = 4; // input pullup. LOW - proximity sensor went off
+int PIN_ENABLE = 6; // instatly stops or starts the motor
+
+// constants: status of the switch, also routines   
+int DEF_UP = 1;
+int DEF_OFF = 0;
+int DEF_DOWN = 2;
+
+// current and previous states
+int prevState = DEF_OFF;
+
+int currentRoutine = DEF_OFF; // resuing DEF_ constants for routine names
+int routineStep = 0;
+
+void setup() {  
+  // Serial.begin(9600);
+  myStepper.setSpeed(30);  // option 2
+  
+  pinMode(PIN_TOP, INPUT_PULLUP);
+  pinMode(PIN_BOTTOM, INPUT_PULLUP);  
+  pinMode(PIN_END_OF_TRACK, INPUT_PULLUP);
+  pinMode(PIN_ENABLE, OUTPUT); 
+  digitalWrite(PIN_ENABLE, LOW); 
+}
+
+void loop() {
+  // get the sensor value
+  int currentState = readSwitchStatus();
+  
+  // if state changed
+  if (currentState != prevState){
+    prevState = currentState;            
+    halt();
+    if (currentState == DEF_OFF)
+      return;
+    else
+      routine(currentState, true);
+  }else{
+    if (currentState != DEF_OFF && currentRoutine != DEF_OFF)
+      routine(currentRoutine, false);  
+  }    
+  
+}
+
+
+void routine(int routineId, bool newCycle){
+  int maxSteps = routineId == DEF_UP? MAX_STEPS_UP : MAX_STEPS_DOWN;
+  if (currentRoutine != routineId)
+    currentRoutine = routineId; 
+    
+  if (routineId == DEF_UP && digitalRead(PIN_END_OF_TRACK)==LOW){
+      halt();    
+      return;
+  }
+  if (newCycle)
+    digitalWrite(PIN_ENABLE, HIGH); 
+    
+  if (routineId == DEF_UP)
+    myStepper.step(1); 
+  else
+    myStepper.step(-1);   
+  
+  routineStep++;
+  if (routineStep >= maxSteps)
+    halt();
+}
+
+int halt(){
+  digitalWrite(PIN_ENABLE, LOW); 
+  currentRoutine=DEF_OFF;
+  routineStep = 0;
+}
+
+int readSwitchStatus(){
+  int val = digitalRead(PIN_TOP);
+  if (val == LOW){
+    return DEF_UP;
+  }
+  
+  val = digitalRead(PIN_BOTTOM);
+  if (val == LOW){
+    return DEF_DOWN;
+  }    
+  
+  return DEF_OFF;
+}
